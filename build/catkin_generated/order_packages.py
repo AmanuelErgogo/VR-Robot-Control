2037c25fde459c73992b33005d09fcf3714e5ded
Amanuel Tadesse <omenoftech@gmail.com>
2022-12-06 15:25:37 +0100

    First commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/aman/unity_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/aman/catkin_ws/devel;/home/aman/ws_moveit/devel;/opt/ros/noetic'.split(';') if '/home/aman/catkin_ws/devel;/home/aman/ws_moveit/devel;/opt/ros/noetic' != '' else []
---
+++ devel/share/unity_robotics_demo_msgs/cmake/unity_robotics_demo_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(unity_robotics_demo_msgs_MESSAGE_FILES "/home/aman/unity_ws/src/unity_robotics_demo_msgs/msg/PosRot.msg;/home/aman/unity_ws/src/unity_robotics_demo_msgs/msg/UnityColor.msg")
+set(unity_robotics_demo_msgs_SERVICE_FILES "/home/aman/unity_ws/src/unity_robotics_demo_msgs/srv/ObjectPoseService.srv;/home/aman/unity_ws/src/unity_robotics_demo_msgs/srv/PositionService.srv")
